b3d25f8e2f79db33a5354e32926c31ffbb848c5d
salmmanfred <32799244+salmmanfred@users.noreply.github.com>
2023-12-11 21:20:01 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,85 @@
+import express from "npm:express@4";
+
+
+const app = express();
+
+const index = await Deno.readTextFile("index.html");
+
+const map1 = new Map();
+
+app.get("/", (_, res) => {
+    //const index = Deno.readTextFileSync("index.html");
+    res.set('Content-Type', 'text/html');
+    res.send(index);
+});
+
+app.use(express.json());
+
+
+app.post("/new", (request, response) => {
+    const info = request.body;
+   
+
+    let stringOk = false;
+    let str = ""
+    while (!stringOk){
+        const strt = genRandonString(4);
+        if (!map1.has(strt)){
+            stringOk = true;
+            str = strt;
+        }
+    } 
+    if (str == ""){
+        response.send("err");
+        return
+    }
+    map1.set(str,info);
+    map1.set(str+"_date",Date.now());
+
+
+    response.send(str);
+});
+
+app.param('token', function(req, _, next, token) {
+    
+  
+    req.token = token;
+    next();
+  });
+
+app.get("/ret/:token", (request, response) => {
+    const st = request.token
+
+    const s = map1.get(st);
+    const date = map1.get(st+"_date");
+    if (Date.now()-date >= 60000){
+        response.send(JSON.stringify({
+            "text": "TooOLD"
+        }));
+        map1.delete(st);
+        map1.delete(st+"_date");
+
+        return
+
+    }
+    map1.delete(st);
+    map1.delete(st+"_date");
+    
+    response.send(s);
+
+});
+
+
+
+app.listen({ hostname: "192.168.68.102", port: 8000 });
+
+
+function genRandonString(length) {
+    const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    const charLength = chars.length;
+    let result = '';
+    for (let i = 0; i < length; i++ ) {
+       result += chars.charAt(Math.floor(Math.random() * charLength));
+    }
+    return result;
+ }
