d0e4bd2a5aa335ec8edb268572bd1675edc773eb
salmmanfred <32799244+salmmanfred@users.noreply.github.com>
2024-04-19 10:14:28 +0200

    New server

---
+++ server/src/main.rs
@@ -0,0 +1,22 @@
+#[macro_use]
+extern crate lazy_static;
+use axum::{body::{Body, BodyDataStream}, http::{header, StatusCode}, response::{Html, IntoResponse}, routing::{get, post}, Router};
+use tokio_util::io::ReaderStream;
+mod image_hand;
+mod paste_hand;
+#[tokio::main]
+async fn main() {
+    println!("Starting server");
+    let app = Router::new()
+        // `GET /` goes to `root`
+        .route("/", get(root))
+        .route("/favicon", get(image_hand::image))
+        .route("/new", post(paste_hand::post_token));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> Html<String>{
+    Html(include_str!("../../index.html").to_string())
+}
+
---
+++ server/src/paste_hand.rs
@@ -0,0 +1,69 @@
+use std::{collections::HashMap, io::empty, sync::Mutex, thread, time};
+
+use axum::extract::Json;
+use rand::{distributions::Alphanumeric, Rng};
+use serde::{Deserialize, Serialize};
+
+lazy_static! {
+    static ref TOKENS: Mutex<HashMap<String, Paste>> = {
+        let m = HashMap::new();
+        Mutex::new(m)
+    };
+}
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Paste{
+    save_time: i64,
+    text: String,
+}
+
+impl Paste{
+    pub fn new(s: String, i: i64)->Self{
+        Self { save_time: i, text: s }
+    }
+}
+pub async fn post_token(Json(item): Json<(Paste)>)->String{
+    //let item = Paste::new(item.0,item.1);
+    let mut ttl = item.save_time;
+    if ttl > 10{
+        ttl = 10;
+    }
+    if ttl < 1{
+        ttl = 1;
+    }
+    let key = create_key(item);
+    
+    delete_after(key.clone(), ttl as u64);
+
+   
+    return key
+}
+
+fn create_key(paste: Paste)->String{
+    let mut tl = TOKENS.lock().unwrap();
+    let mut key = "".to_string();
+    while key == ""{
+        let s: String = rand::thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(4)
+            .map(char::from)
+            .collect();
+        if !tl.contains_key(&s){
+            key = s;
+        }
+    }
+    tl.insert(key.clone(), paste);
+    println!("{:#?}", tl);
+    drop(tl);
+    return key
+}
+
+fn delete_after(key: String, ttl: u64){
+    thread::spawn(move || {
+        let time = time::Duration::from_secs(ttl*60);
+        thread::sleep(time);
+        let mut tl = TOKENS.lock().unwrap();
+
+        tl.remove(&key);
+        drop(tl)
+    });
+}
