c01408ba3d2bdc179c3e5e654b49ae494e0e7c7b
salmmanfred <32799244+salmmanfred@users.noreply.github.com>
2024-06-26 22:44:09 +0200

    Fix.

--- server/src/main.rs
+++ server/src/main.rs
@@ -5,6 +5,11 @@ mod image_hand;
 mod paste_hand;
 #[tokio::main]
 async fn main() {
+    //This one doesnt make sense at first but there is a really good reason for it 
+    //This line at the start of the program writes the favicon file to the root directory of the server
+    //and since im too lazy to find the actual path 
+    // I use "include_bytes" which startpoint (./) is where the file (main.rs) is.
+    // it then writes it to the local directory of the execution which is god knows where
     openfile::write_file_bytes("./favicon.ico", include_bytes!("../favicon.ico").to_vec()).unwrap();
     println!("Starting server");
     //Removes information thats only suppose to be in the experimental version and so on
